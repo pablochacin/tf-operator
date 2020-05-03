aec995aad5c6c2111344ad35950084137d66f3f0
Pablo Chacin <pablochacin@gmail.com>
2020-05-03 12:31:48 +0200

    Refactor into Runner interface
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/cmdrunner/cmdrunner.go
+++ pkg/cmdrunner/cmdrunner.go
@@ -1,17 +1,19 @@
 package cmdrunner
 
 import (
-    "errors"
-    "fmt"
+	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 )
 
-// CmdEnvironment defines the environment options for executing a command
-type CmdEnvironment struct {
-    InheritEnv bool
-    WorkDir string
-    Env     map[string]string
+// Runner a command runner
+type Runner interface {
+	Run(shellCmd string, args ...string) (*CmdResult, error)
+	SetWorkDir(path string) error
+	SetInheritEnv(inherit bool)
+	SetEnv(env map[string]string)
+	AddEnv(varibale string, value string)
 }
 
 // CmdResult contains the result of executing a command. Output contains the
@@ -21,37 +23,66 @@ type CmdResult struct {
 	Output   string
 }
 
+// cmdEnvironment defines the environment options for executing a command
+type cmdEnvironment struct {
+	InheritEnv bool
+	WorkDir    string
+	Env        map[string]string
+}
+
+func New() Runner {
+	return &cmdEnvironment{}
+}
+
 // Run runs a shell cmd in an execution environment
-func (e *CmdEnvironment)Run(shellCmd string, args ...string) (*CmdResult, error) {
+func (e *cmdEnvironment) Run(shellCmd string, args ...string) (*CmdResult, error) {
 	cmd := exec.Command(shellCmd, args...)
 
-    if e.WorkDir != "" {
-	    cmd.Dir = e.WorkDir
-    }
+	if e.WorkDir != "" {
+		cmd.Dir = e.WorkDir
+	}
 
-    // setup environement
-    env := []string{}
-    if e.InheritEnv {
-        env = append(env, os.Environ()...)
-    }
-    if e.Env != nil {
-        for k,v := range(e.Env) {
-            env = append(env, fmt.Sprintf("%s=\"%s\"", k, v))
-        }
-    }
-    cmd.Env = env
+	// setup environement
+	env := []string{}
+	if e.InheritEnv {
+		env = append(env, os.Environ()...)
+	}
+	if e.Env != nil {
+		for k, v := range e.Env {
+			env = append(env, fmt.Sprintf("%s=\"%s\"", k, v))
+		}
+	}
+	cmd.Env = env
 
-    output, err := cmd.CombinedOutput()
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-        if errors.Is(err, &exec.ExitError{}) {
-		    return nil, err
-        }
+		if errors.Is(err, &exec.ExitError{}) {
+			return nil, err
+		}
 	}
 
 	result := &CmdResult{
-                ExitCode: cmd.ProcessState.ExitCode(),
-                Output: string(output),
-              }
+		ExitCode: cmd.ProcessState.ExitCode(),
+		Output:   string(output),
+	}
 
-    return result, nil
+	return result, nil
+}
+
+func (e *cmdEnvironment) SetEnv(env map[string]string) {
+	e.Env = env
+}
+
+func (e *cmdEnvironment) AddEnv(variable string, value string) {
+	e.Env[variable] = value
+}
+
+func (e *cmdEnvironment) SetWorkDir(path string) error {
+	// TODO: check path is valid
+	e.WorkDir = path
+	return nil
+}
+
+func (e *cmdEnvironment) SetInheritEnv(inherit bool) {
+	e.InheritEnv = inherit
 }
--- pkg/cmdrunner/cmdrunner_test.go
+++ pkg/cmdrunner/cmdrunner_test.go
@@ -5,7 +5,6 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
 )
 
 func TestCmdRunner(t *testing.T) {
@@ -15,97 +14,96 @@ func TestCmdRunner(t *testing.T) {
 
 var _ = Describe("Command Runner", func() {
 	var (
-        result  *CmdResult
-        err     error
+		result *CmdResult
+		err    error
 	)
-
 
 	Context("Run Job with default enviroment", func() {
 
-        BeforeEach(func() {
-		    env := &CmdEnvironment{}
-	        result, err = env.Run("echo", "-n", "testing echo")
-	    })
+		BeforeEach(func() {
+			runner := New()
+			result, err = runner.Run("echo", "-n", "testing echo")
+		})
 
 		It("Should not fail", func() {
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(result).ToNot(BeNil())
 		})
 
-        It("Should return non error exit code", func(){
-            Expect(result.ExitCode).To(Equal(0))
-        })
+		It("Should return non error exit code", func() {
+			Expect(result.ExitCode).To(Equal(0))
+		})
 
 		It("should return stdout", func() {
 			Expect(result.Output).To(Equal("testing echo"))
 		})
 	})
 
-    Context("Capture stderr", func() {
-        BeforeEach(func(){
-            env := &CmdEnvironment{}
-            result, err = env.Run("sh", "-c", "echo -n >&2 'testing echo'")
-        })
+	Context("Capture stderr", func() {
+		BeforeEach(func() {
+			runner := New()
+			result, err = runner.Run("sh", "-c", "echo -n >&2 'testing echo'")
+		})
 
 		It("Should not fail", func() {
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(result).NotTo(BeNil())
 		})
 
-        It("Should capture stderr", func(){
-            Expect(result.Output).To(Equal("testing echo"))
-        })
-    })
+		It("Should capture stderr", func() {
+			Expect(result.Output).To(Equal("testing echo"))
+		})
+	})
 
-    Context("Run job with environment variables", func(){
-	    BeforeEach(func() {
-		    env :=  &CmdEnvironment{
-                        InheritEnv: false,
-                        Env: map[string]string{"FOO": "BAR"},
-                    }
-            result, err = env.Run("env")
-	    })
+	Context("Run job with environment variables", func() {
+		BeforeEach(func() {
+			runner := New()
+			runner.SetInheritEnv(false)
+			runner.SetEnv(map[string]string{"FOO": "BAR"})
+			result, err = runner.Run("env")
+		})
 
-        It("Should not fail", func(){
-            Expect(err).To(BeNil())
-            Expect(result).NotTo(BeNil())
-        })
+		It("Should not fail", func() {
+			Expect(err).To(BeNil())
+			Expect(result).NotTo(BeNil())
+		})
 
-        It("Should return env variables", func() {
-            Expect(result.Output).To(Equal("FOO=\"BAR\"\n"))
-        })
-    })
+		It("Should return env variables", func() {
+			Expect(result.Output).To(Equal("FOO=\"BAR\"\n"))
+		})
+	})
 
-    Context("Capture return code", func(){
-        BeforeEach(func(){
-            env := &CmdEnvironment{}
-            result, err = env.Run("/bin/false")
-        })
+	Context("Capture return code", func() {
+		BeforeEach(func() {
+			runner := New()
+			result, err = runner.Run("/bin/false")
+		})
 
-        It("Should not fail", func(){
-            Expect(err).To(BeNil())
-            Expect(result).NotTo(BeNil())
-        })
+		It("Should not fail", func() {
+			Expect(err).To(BeNil())
+			Expect(result).NotTo(BeNil())
+		})
 
-        It("Should return exit code", func() {
-            Expect(result.ExitCode).To(Equal(1))
-        })
-     })
+		It("Should return exit code", func() {
+			Expect(result.ExitCode).To(Equal(1))
+		})
+	})
 
-    Context("Changes the working dir", func(){
-        BeforeEach(func(){
-            env := &CmdEnvironment{WorkDir: "/tmp"}
-            result, err = env.Run("pwd")
-        })
+	Context("Changes the working dir", func() {
+		BeforeEach(func() {
+			runner := New()
+			runner.SetWorkDir("/tmp")
+			result, err = runner.Run("pwd")
+		})
 
-        It("Should not fail", func(){
-            Expect(err).To(BeNil())
-            Expect(result).NotTo(BeNil())
-        })
+		It("Should not fail", func() {
+			Expect(err).To(BeNil())
+			Expect(result).NotTo(BeNil())
+		})
 
-        It("Should run in working directory", func() {
-            Expect(result.Output).To(Equal("/tmp\n"))
-        })
- 
-    })
- })
+		It("Should run in working directory", func() {
+			Expect(result.Output).To(Equal("/tmp\n"))
+		})
+
+	})
+})
